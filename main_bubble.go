8090ee563294c63921cb0471e407181539d00572
HennningS <79580243+HennningS@users.noreply.github.com>
2022-04-01 11:34:46 +0200

    Add files via upload

---
+++ main_bubble.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/digitnow/wengrow/bubble"
+)
+
+func main() {
+	array := []int{4, 2, 7, 1, 3}
+	fmt.Println(array)
+	fmt.Println(bubble.BubbleSort(array))
+}
